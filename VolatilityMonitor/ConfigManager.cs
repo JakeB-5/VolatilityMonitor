f89c7681648ebfa0338f9a68909ec301b8e7dcab
JakeB <quarries.jake@gmail.com>
2025-05-13 21:39:22 +0900

    Add ConfigManager - read from config.ini

---
+++ VolatilityMonitor/ConfigManager.cs
@@ -0,0 +1,47 @@
+﻿namespace VolatilityMonitor;
+
+public class ConfigManager
+{
+    private readonly static string DataPath = @".\";
+    private static readonly Lazy<ConfigManager> Lazy = new(() => new ConfigManager());
+    public static ConfigManager Instance => Lazy.Value;
+    private readonly Dictionary<string, string?> _settings = new();
+
+    protected ConfigManager()
+    {
+        LoadConfigData();
+    }
+
+    private void LoadConfigData()
+    {
+        using StreamReader sr = new StreamReader(Path.Combine(DataPath, "data.ini"));
+        string? line;
+        String delimStr = "=";
+        char[] delimiter = delimStr.ToCharArray();
+        while ((line = sr.ReadLine()) != null)
+        {
+            string?[] strData = line.Split(delimiter);
+            _settings.Add(strData[0], strData[1]);
+        }
+    }
+    
+    private string? GetSetting(ConfigKey key)
+    {
+        return _settings[Enum.GetName(typeof(ConfigKey), key)];
+    }
+
+    public static string? Get(ConfigKey key, string defaultValue = "")
+    {
+        return Instance.GetSetting(key) ?? defaultValue;
+    }
+}
+
+
+public enum ConfigKey
+{
+    TelegramBotToken,
+    TelegramChannelId,
+    CandleInterval,
+    AlertCooldown,
+}
+
